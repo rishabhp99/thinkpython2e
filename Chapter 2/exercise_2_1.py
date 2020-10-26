22a2a9923c5f8383ebd70064f02ebb0a4d126483
Rishabh Priyadarshi <rishabhpriyadarshi.discovery@gmail.com>
2020-10-27 00:29:31 +0530

    Add files via upload

---
+++ Chapter 2/exercise_2_1.py
@@ -0,0 +1,13 @@
+# Type the following statements in the Python interpreter to see what they do:
+# 5
+# x = 5
+# x + 1
+# Now put the same statements into a script and run it. What is the output? Modify the script by
+# transforming each expression into a print statement and then run it again.
+
+
+print(5)
+x = 5
+print(x)
+print(x + 1)
+
