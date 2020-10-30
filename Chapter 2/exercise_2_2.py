474fb2da8261f02163dbca98336d695b1eaa8f7c
Ruchi Sharma <ruchis1998@gmail.com>
2020-10-30 23:57:01 +0530

    Updated exercise 2.2

--- Chapter 2/exercise_2_2.py
+++ Chapter 2/exercise_2_2.py
@@ -1,25 +1,11 @@
-# Assume that we execute the following assignment statements:
-# width = 17
-# height = 12.0
-# delimiter = '.'
-# For each of the following expressions, write the value of the expression and the type (of the value of
-# the expression).
-# 1. width/2
-# 2. width/2.0
-# 3. height/3
-# 4. 1 + 2 * 5
-# 5. delimiter * 5
+# Practice using the Python interpreter as a calculator:
+# 1. The volume of a sphere with radius r is 4/3πr^3. What is the volume of a sphere with radius 5?
+# 2. Suppose the cover price of a book is $24.95, but bookstores get a 40% discount. Shipping costs
+# $3 for the first copy and 75 cents for each additional copy. What is the total wholesale cost for
+# 60 copies?
+# 3. If I leave my house at 6:52 am and run 1 mile at an easy pace (8:15 per mile), then 3 miles at
+# tempo (7:12 per mile) and 1 mile at easy pace again, what time do I get home for breakfast?
 
-width = 17
-height = 12.0
-delimiter = '.'
+# 1.
 
-print(width/2, "type: ", type(width/2))
-
-print(width/2.0, "type: ", type(width/2.0))
-
-print(height/3, "type: ", type(height/3))
-
-print(1 + 2 * 5, "type: ", type(1 + 2 * 5))
-
-print(delimiter * 5, "type: ", type(delimiter*5))
+print(4/3 * 3.14 * 5 * 5 * 5)
